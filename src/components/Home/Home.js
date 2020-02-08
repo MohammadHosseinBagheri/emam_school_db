f31e75d4ca9558fc8331065ac4b4d6af66deb36d
MohammadHosseinBagheri <mohammadbagheri228@gmail.com>
2020-02-09 01:45:32 +0330

    home ui

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,14 +1,138 @@
 import React, {Component} from 'react';
 import {View, Text, FlatList} from 'react-native';
 import data from '../../data/data';
-import {Item, Input} from 'native-base';
+import {Item, Input, Card, CardItem, Button} from 'native-base';
 
 class BasicItem extends Component {
   render() {
     const {item} = this.props;
     return (
-      <View style={{flex: 1, backgroundColor: '#4a6572', margin: 5}}>
-        <Text>{item.id}</Text>
+      <View
+        style={{
+          flex: 1,
+          flexDirection: 'column',
+          backgroundColor: '#344955',
+          margin: 20,
+          borderRadius: 20,
+          elevation:5
+        }}>
+        <Card
+          style={{
+            flex: 1,
+            flexDirection: 'column',
+            backgroundColor: '#4a6572',
+            elevation: 5,
+            borderRadius: 20,
+            borderTopWidth: 0,
+            borderRightWidth: 0,
+            borderBottomWidth: 0,
+            borderLeftWidth: 0,
+          }}>
+          <CardItem
+            cardBody
+            style={{
+              flex: 1,
+              justifyContent: 'flex-end',
+              margin: 10,
+              backgroundColor: '#4a6572',
+            }}>
+            <Text
+              style={{
+                fontFamily: 'IRANSansMobile_Bold',
+                fontSize: 16,
+                color: 'white',
+              }}>
+              نام : {item.first_name}
+              {'\n'}
+              نام خانوادگی : {item.last_name}
+            </Text>
+          </CardItem>
+          <CardItem
+            style={{
+              flex: 1,
+              flexDirection: 'row',
+              justifyContent: 'flex-end',
+              backgroundColor: '#4a6572',
+            }}>
+            <Button
+              style={{
+                backgroundColor: '#00E676',
+                width: 50,
+                margin: 10,
+                justifyContent: 'center',
+                borderRadius: 10,
+                elevation: 5,
+              }}>
+              <Text style={{fontFamily: 'IRANSansMobile_Medium'}}>تماس</Text>
+            </Button>
+            <Text
+              style={{
+                textAlign: 'right',
+                fontFamily: 'IRANSansMobile',
+                fontSize: 16,
+                color: 'white',
+              }}>
+              شماره موبایل : {item.mobile_phone}
+            </Text>
+          </CardItem>
+          <CardItem
+            style={{
+              flex: 1,
+              flexDirection: 'row',
+              justifyContent: 'flex-end',
+              backgroundColor: '#4a6572',
+            }}>
+            <Button
+              style={{
+                backgroundColor: '#ff1744',
+                width: 50,
+                margin: 10,
+                justifyContent: 'center',
+                borderRadius: 10,
+                elevation: 5,
+              }}>
+              <Text style={{fontFamily: 'IRANSansMobile_Medium'}}>تماس</Text>
+            </Button>
+            <Text
+              style={{
+                textAlign: 'right',
+                fontFamily: 'IRANSansMobile',
+                fontSize: 16,
+                color: 'white',
+              }}>
+              شماره خانه : {item.home_phone}
+            </Text>
+          </CardItem>
+          <CardItem
+            style={{
+              flex: 1,
+              flexDirection: 'row',
+              justifyContent: 'flex-end',
+              backgroundColor: '#4a6572',
+              borderRadius:20
+            }}>
+            <Button
+              style={{
+                backgroundColor: '#F9AA33',
+                width: 50,
+                margin: 10,
+                justifyContent: 'center',
+                borderRadius: 10,
+                elevation: 5,
+              }}>
+              <Text style={{fontFamily: 'IRANSansMobile_Medium'}}>تماس</Text>
+            </Button>
+            <Text
+              style={{
+                textAlign: 'right',
+                fontFamily: 'IRANSansMobile',
+                fontSize: 16,
+                color: 'white',
+              }}>
+              شماره موبایل پدر : {item.father_mobile}
+            </Text>
+          </CardItem>
+        </Card>
       </View>
     );
   }
@@ -20,7 +144,6 @@ class Home extends Component {
     this.state = {
       student: '',
       data: [],
-      load: null,
     };
   }
   _listEmptyComponent = () => {
